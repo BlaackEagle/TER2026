3c53f9571c1870d1bad967eb5cba6d29940424f0
TiryWorld <samy.sadeg@etu.umontpellier.fr>
2026-04-27 15:39:00 +0200

    ajout d'une boucle pour avoir nos résultats sous forme de CSV

--- CV_LLM_app/app.py
+++ CV_LLM_app/app.py
@@ -7,6 +7,7 @@ from services.pdf_parser import extraire_intelligent
 from services.cosinus_similarity import pertinence
 from services.chunking import fct_de_chunk
 
+import csv
 import chromadb
 
 app = Flask(__name__)
@@ -47,7 +48,7 @@ def analyse_cv():
         text_extrait = extraire_intelligent(file)
         if text_extrait:
             TEXTES_COMPLETS_GLOBAUX[filename] = text_extrait
-            chunks = fct_de_chunk(text_extrait, taille=30, mode="mots", tag="", overlap=5)
+            chunks = fct_de_chunk(text_extrait, taille=40, mode="mots", tag="", overlap=5)
             vdt.vectoriser_liste_text(chunks, filename, collection)
             nouveaux_fichiers_traites += 1
         else:
@@ -56,12 +57,16 @@ def analyse_cv():
         return jsonify({'error': 'Aucun fichier en mémoire. Envoyez des CVs !'}), 400
     bdd_score = vdt.calculer_scores_bdd(collection, vecteur_prompt)
     vdt.note_final_bdd(bdd_score)
+    lignes_csv = []
+    vus_csv = set()
     data_pour_le_front = []
     vus = set()
     for ligne in bdd_score :
         nom = ligne['nomFichier']
         if nom not in vus:
             texte_entier = TEXTES_COMPLETS_GLOBAUX.get(nom, 'null')
+            lignes_csv.append({'nom_fichier': nom, 'score': ligne['score']})
+            vus_csv.add(nom)
             data_pour_le_front.append({
                 'nom_fichier': ligne['nomFichier'],
                 'meilleur_extrait': ligne['texte'],
@@ -69,7 +74,12 @@ def analyse_cv():
                 'texte_fichier': texte_entier
             })
             vus.add(ligne['nomFichier'])
-
+    nom_du_csv = "resultats_40.csv" # <--- À CHANGER À CHAQUE TEST
+    with open(nom_du_csv, mode='w', newline='', encoding='utf-8') as fichier_csv:
+        writer = csv.DictWriter(fichier_csv, fieldnames=['nom_fichier', 'score'])
+        writer.writeheader()
+        for row in lignes_csv:
+            writer.writerow(row)
     return jsonify({
         'message': 'Analyse terminée',
         'count' : len(data_pour_le_front),
