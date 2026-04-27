b1604691cfc133de50a0484d2e4a9878491ca410
jaouad <jaouad.kabouri-ouzennou@etu.umontpellier.fr>
2026-04-27 16:36:46 +0200

    mise sous forme de classe de pdf_parser et adaptation au serveur

--- CV_LLM_app/services/pdf_parser.py
+++ CV_LLM_app/services/pdf_parser.py
@@ -1,30 +1,22 @@
 import os 
-from pypdf import PdfReader
+#from pypdf import PdfReader   //ne sert plus dcp
 import pdfplumber
 
-def cleaner_texte2(texte) :  
-    if not texte:
-        return ""
-    return texte.strip()
+
+class PdfParser:
+    def cleaner_texte2(self, texte) :  
+        if not texte:
+            return ""
+        return texte.strip()
 
 
-#Version avec pdfPlumber qui marche tellement mieux qu'avec PdfReader
-def extraire_intelligent(pdf_object):
-    texte_complet = ""
-    with pdfplumber.open(pdf_object) as pdf:
-        for page in pdf.pages:
-            texte = page.extract_text(layout=True)
-            if texte:
-                texte_complet += texte + " "
+    #Version avec pdfPlumber qui marche tellement mieux qu'avec PdfReader
+    def extraire_intelligent(self, pdf_object):
+        texte_complet = ""
+        with pdfplumber.open(pdf_object) as pdf:
+            for page in pdf.pages:
+                texte = page.extract_text(layout=True)
+                if texte:
+                    texte_complet += texte + " "
 
-    return cleaner_texte2(texte_complet)
-
-if __name__ == "__main__":
-    print("--------------")
-    test_path = "../uploads/CV_Samy.pdf"
-
-    text = extraire_text_pdf(test_path)
-    print(text)
-    with open("../uploads/CV_Samy.txt", "w", encoding="utf-8") as fichier:
-        fichier.write(text)
-    print("----------")
+        return self.cleaner_texte2(texte_complet)
